eb7df989174191a20e7d73e8346e733307a439df
SinekaRamu <sineka2001cud@gmail.com>
2023-11-29 21:33:04 +0530

    init commit

---
+++ app/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="navbar">
+      <div className="logo">
+       <h1>Sineka here..</h1>
+        Portfolio
+        </div>
+      <ul className="navTool">
+        <li>About</li>
+        <li>Projects</li>
+        <li>Serives</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
